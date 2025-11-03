d628bdc140510c8c9a1b0e4b6a8754effff9a9a8
Peixotim <pedropeixotovz@gmail.com>
2025-11-03 14:55:50 -0300

    feat: Adiciona duas novas rotas , rota de criação e rota de procurar todas emoções

--- src/emotion/emotion.controller.ts
+++ src/emotion/emotion.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { EmotionService } from './emotion.service';
+import { Emotion } from './entity/emotion.entity';
+import { EmotionCreate } from './DTOs/emotion.create.dto';
 
 @Controller('emotion')
 export class EmotionController {
-  constructor(private readonly emotionServices: EmotionService) {}
+  constructor(private readonly emotionService: EmotionService) {}
+
+  @Post()
+  public async create(@Body() request: EmotionCreate): Promise<Emotion> {
+    return this.emotionService.create(request);
+  }
+  @Get()
+  public async find(): Promise<Emotion[]> {
+    return this.emotionService.findAll();
+  }
 }
