ad2ab4dd8c52c4fc4733effc08c9851727d9b380
Peixotim <pedropeixotovz@gmail.com>
2025-11-03 14:51:14 -0300

    Feat: Adiciona o serviço  de listar todas as entidades +  o serviço de criação de entidades

--- src/emotion/emotion.service.ts
+++ src/emotion/emotion.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Emotion } from './entity/emotion.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EmotionCreate } from './DTOs/emotion.create.dto';
+import { HttpStatusCode } from 'axios';
 
 @Injectable()
-export class EmotionService {}
+export class EmotionService {
+  constructor(
+    @InjectRepository(Emotion)
+    private readonly emotionRepository: Repository<Emotion>,
+  ) {}
+
+  public async create(create: EmotionCreate): Promise<Emotion> {
+    try {
+      const newEmotion: Emotion = this.emotionRepository.create(create);
+      const savedEmotion: Emotion =
+        await this.emotionRepository.save(newEmotion);
+
+      return savedEmotion;
+    } catch (error: unknown) {
+      console.error(
+        '[EmotionService][create] Error:',
+        error instanceof Error
+          ? {
+              message: error.message,
+              stack: error.stack,
+            }
+          : error,
+      );
+      throw new HttpException(
+        `Failure to create a new emotion ${error instanceof Error ? error.message : 'unknown error'}`,
+        HttpStatusCode.InternalServerError,
+      );
+    }
+  }
+
+  public async findAll(): Promise<Emotion[]> {
+    if ((await this.emotionRepository.find()).length === 0) {
+      throw new HttpException(
+        `Error, there is no emotion registered in our database !`,
+        HttpStatusCode.NotFound,
+      );
+    }
+
+    try {
+      const find: Emotion[] = await this.emotionRepository.find();
+      return find;
+    } catch (error: unknown) {
+      console.error(
+        '[EmotionService][find] Error:',
+        error instanceof Error
+          ? {
+              message: error.message,
+              stack: error.stack,
+            }
+          : error,
+      );
+
+      throw new HttpException(
+        `Failure to seek emotions ${error instanceof Error ? error.message : 'unknown error'}`,
+        HttpStatusCode.InternalServerError,
+      );
+    }
+  }
+}
